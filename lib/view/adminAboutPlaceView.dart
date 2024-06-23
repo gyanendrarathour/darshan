44b79a01f250ccf3ad276fda31108b617c1f09c3
Gyanendra singh <krishurathour1992@gmail.com>
2024-06-23 23:54:21 +0530

    Add about the place feature

--- lib/view/adminAboutPlaceView.dart
+++ lib/view/adminAboutPlaceView.dart
@@ -19,6 +19,73 @@ class Adminaboutplaceview extends StatefulWidget {
 class _AdminaboutplaceviewState extends State<Adminaboutplaceview> {
   final CollectionReference _collectionReference =
       FirebaseFirestore.instance.collection('states');
+  TextEditingController inCityController = TextEditingController();
+  TextEditingController aboutPlaceController = TextEditingController();
+
+  Future<void> _addDetails() async {
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        useSafeArea: true,
+        context: context,
+        builder: (context) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                TextField(
+                  controller: inCityController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "What is in this city"),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: aboutPlaceController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Enter about the place"),
+                  maxLines: 5,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                  child: ElevatedButton(
+                      onPressed: () async {
+                        String inCity = inCityController.text;
+                        String aboutPlace = aboutPlaceController.text;
+                        await _collectionReference
+                            .doc(widget.stateId)
+                            .collection("places").doc(widget.placeId).collection('about_place')
+                            .add({
+                          'inCity': inCity,
+                          'aboutPlace': aboutPlace
+                        });
+                        inCityController.text = "";
+                        aboutPlaceController.text = "";
+                        Navigator.pop(context);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text(
+                                    'About the Place has been added successfully...')));
+                      },
+                      child: const Text(
+                        'Add',
+                        style: TextStyle(fontSize: 20),
+                      )),
+                )
+              ],
+            ),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,10 +126,10 @@ class _AdminaboutplaceviewState extends State<Adminaboutplaceview> {
                               ),
                             ),
                             ListTile(
-                              title: Text(_placeData["stations"].toString()),
+                              title: Text(_placeData["inCity"].toString()),
                             ),
                             ListTile(
-                              title: Text(_placeData["about"].toString()),
+                              title: Text(_placeData["aboutPlace"].toString()),
                             ),
                           ],
                         ),
@@ -74,6 +141,10 @@ class _AdminaboutplaceviewState extends State<Adminaboutplaceview> {
                 );
               }
             }),
+            floatingActionButton: FloatingActionButton(onPressed: () async{
+              await _addDetails();
+            },
+            child: Icon(Icons.add), backgroundColor: Colors.blue,),
       ),
     );
   }
